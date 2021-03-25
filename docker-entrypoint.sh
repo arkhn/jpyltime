ff210649df7bef9c9c67a55c2d5d3255a259dce3
clementjumel <clement.jumel@gmail.com>
2021-03-25 11:33:52 +0100

    :wrench: Finalize config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-uvicorn jpyltime.app:app --host="0.0.0.0" --port=7000 --root-path=${API_ROOT_PATH}
+poetry run uvicorn jpyltime.app:app --host="0.0.0.0" --port=7000 --root-path=${API_ROOT_PATH}
