76dbf0bb7b83d9d52620c14026cc95d747b5d112
Louise Naudin <naudin.louise@gmail.com>
2021-03-30 10:51:56 +0200

    :white_mark_check: Add fixture in tests

--- tests/postprocessing_test.py
+++ tests/postprocessing_test.py
@@ -1,33 +1,65 @@
 import json
+
 import pandas as pd
+import pytest
+
 from jpyltime.postprocessing_fhir2ds import FHIR2DS_Postprocessing
 from jpyltime.utils import Attribute
 
-import pytest
 
 @pytest.fixture
-def attribute_file():
-    attribute_file = "documents/attributes_mapping.json"
-    return attribute_file
+def map_attributes():
+    attribute_file = "jpyltime/documents/attributes_mapping.json"
+    with open(attribute_file, "r") as f:
+        map_attributes = json.loads(f.read())
+    return map_attributes
 
+
+@pytest.fixture
 def example_dataframe():
-    data = [["8392", 'tom', 10,"ICD", 22, "mg", 50, "kg", "2019-20-20"], ["8392", 'tom', 1, "ICD",8493, "L", 50, "kg"], ["8392", 'nick', 10,"ICD", 22, "mg", 90, "kg"], ["9382", 'julie', 1,"ICD", 38, "L", 92, "kg"], ["3728",'john', 10, "ICD",22, "mg" ,20, "kg"]]   
-    # Create the pandas DataFrame 
-    df = pd.DataFrame(data, columns = ["Patient:from_id", 'Patient.name.given', 'MedicationRequest:from_id', "MedicationRequest.medicationCodeableConcept.coding.display",'MedicationRequest.dosageInstruction.doseAndRate.doseQuantity.value','MedicationRequest.dosageInstruction.doseAndRate.doseQuantity.unit',
-        'Weight.valueQuantity.value',
-        'Weight.valueQuantity.unit', 'Patient.birthDate'])
+    data = [
+        ["8392", "tom", 10, "ICD", 22, "mg", 50, "kg", "2019-20-20"],
+        ["8392", "tom", 1, "ICD", 8493, "L", 50, "kg"],
+        ["8392", "nick", 10, "ICD", 22, "mg", 90, "kg"],
+        ["9382", "julie", 1, "ICD", 38, "L", 92, "kg"],
+        ["3728", "john", 10, "ICD", 22, "mg", 20, "kg"],
+    ]
+    # Create the pandas DataFrame
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "Patient:from_id",
+            "Patient.name.given",
+            "MedicationRequest:from_id",
+            "MedicationRequest.medicationCodeableConcept.coding.display",
+            "MedicationRequest.dosageInstruction.doseAndRate.doseQuantity.value",
+            "MedicationRequest.dosageInstruction.doseAndRate.doseQuantity.unit",
+            "Weight.valueQuantity.value",
+            "Weight.valueQuantity.unit",
+            "Patient.birthDate",
+        ],
+    )
     return df
 
-def test_postprocessing(attribute_file):
-    df = example_dataframe()
-    attributes = [Attribute(official_name="First name", custom_name="Prénom", anonymize = False), Attribute(official_name="Weight", custom_name="Poids", anonymize = False), Attribute(official_name="Medication", custom_name="Médicaments", anonymize = False),Attribute(official_name="Birthdate", custom_name="Anniversaire", anonymize = True)]
-    d_attributes = {attribute.official_name : attribute for attribute in attributes}
 
-    with open(attribute_file, "r") as f:
-        map_attributes =  json.loads(f.read())
-    anonymization_symbol= "*"
-    postprocessing = FHIR2DS_Postprocessing(map_attributes, anonymization_symbol=anonymization_symbol)
-    display_df = postprocessing.postprocess(df, d_attributes)
+@pytest.fixture
+def attributes():
+    attributes = [
+        Attribute(official_name="First name", custom_name="Prénom", anonymize=False),
+        Attribute(official_name="Weight", custom_name="Poids", anonymize=False),
+        Attribute(official_name="Medication", custom_name="Médicaments", anonymize=False),
+        Attribute(official_name="Birthdate", custom_name="Anniversaire", anonymize=True),
+    ]
+    requested_attributes = {attribute.official_name: attribute for attribute in attributes}
+    return requested_attributes
+
+
+def test_postprocessing(map_attributes, example_dataframe, attributes):
+    anonymization_symbol = "*"
+    postprocessing = FHIR2DS_Postprocessing(
+        map_attributes, anonymization_symbol=anonymization_symbol
+    )
+    display_df = postprocessing.postprocess(example_dataframe, attributes)
 
     true_columns = ["Patient:from_id", "Prénom", "Poids", "Médicaments", "Anniversaire"]
     true = pd.DataFrame(
--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -1,11 +1,24 @@
 import json
+
+import pytest
+
 from jpyltime.preprocessing_fhir2ds import FHIR2DS_Preprocessing
 from jpyltime.utils import Attribute
 
 
-def test_add_group_id():
-    attributes = [Attribute(official_name="First name", custom_name="Prénom", anonymize = False), Attribute(official_name="Gender", custom_name="Sexe", anonymize = False), Attribute(official_name="ASAT", custom_name="ASAT", anonymize = False),Attribute(official_name="Potassium", custom_name="Potassium", anonymize = False)]
-    d_attributes = {attribute.official_name : attribute for attribute in attributes}
+@pytest.fixture
+def requested_attributes():
+    attributes = [
+        Attribute(official_name="First name", custom_name="Prénom", anonymize=False),
+        Attribute(official_name="Gender", custom_name="Sexe", anonymize=False),
+        Attribute(official_name="ASAT", custom_name="ASAT", anonymize=False),
+        Attribute(official_name="Potassium", custom_name="Potassium", anonymize=False),
+    ]
+    requested_attributes = {attribute.official_name: attribute for attribute in attributes}
+    return requested_attributes
+
+
+def test_add_group_id(requested_attributes):
     group_attribute_name = "Group"
     group_id = "38de92"
 
@@ -16,8 +29,7 @@ def test_add_group_id():
     assert "INNER JOIN Group ON Group.member = Patient.id" in query
 
 
-
-def test_simple_query():
+def test_simple_query(requested_attributes):
     true = """SELECT Patient.name.given FROM Patient"""
     attributes = [Attribute(official_name="First name", custom_name="Prénom", anonymize=False)]
     requested_attributes = {attribute.official_name: attribute for attribute in attributes}
@@ -25,24 +37,15 @@ def test_simple_query():
     assert true == query
 
 
-
-def test_complex_query():
-    attributes = [Attribute(official_name="First name", custom_name="Prénom", anonymize = False), Attribute(official_name="Gender", custom_name="Sexe", anonymize = False), Attribute(official_name="ASAT", custom_name="ASAT", anonymize = False),Attribute(official_name="Potassium", custom_name="Potassium", anonymize = False)]
-    d_attributes = {attribute.official_name : attribute for attribute in attributes}
-
-    query = FHIR2DS_Preprocessing().generate_sql_query(d_attributes)
+def test_complex_query(requested_attributes):
+    query = FHIR2DS_Preprocessing().generate_sql_query(requested_attributes)
 
     true = """SELECT Patient.name.given, Patient.gender, ASAT.valueQuantity.value, ASAT.valueQuantity.unit, Potassium.valueQuantity.value, Potassium.valueQuantity.unit FROM Patient INNER JOIN Observation as ASAT ON ASAT.subject = Patient.id INNER JOIN Observation as Potassium ON Potassium.subject = Patient.id WHERE ASAT.code = http://loinc.org%7C1920-8 AND Potassium.code = http://loinc.org%7C2823-3"""
     assert query.split() == true.split()
 
-def test_preprocessing():
-    attributes = [Attribute(official_name="First name", custom_name="Prénom", anonymize = False), Attribute(official_name="Gender", custom_name="Sexe", anonymize = False), Attribute(official_name="ASAT", custom_name="ASAT", anonymize = False),Attribute(official_name="Potassium", custom_name="Potassium", anonymize = False)]
-    d_attributes = {attribute.official_name : attribute for attribute in attributes}
 
-    query, updated_attributes, map_attributes = FHIR2DS_Preprocessing().preprocess(d_attributes.copy())
-
-    diff_attributes = set(updated_attributes.keys()).difference(set(d_attributes.keys()))
-    assert len(diff_attributes) == 0
+def test_preprocessing(requested_attributes):
+    query, map_attributes = FHIR2DS_Preprocessing().preprocess(requested_attributes.copy())
 
     true = """SELECT Patient.name.given, Patient.gender, ASAT.valueQuantity.value, ASAT.valueQuantity.unit, Potassium.valueQuantity.value, Potassium.valueQuantity.unit FROM Patient INNER JOIN Observation as ASAT ON ASAT.subject = Patient.id INNER JOIN Observation as Potassium ON Potassium.subject = Patient.id WHERE ASAT.code = http://loinc.org%7C1920-8 AND Potassium.code = http://loinc.org%7C2823-3"""
     assert query.split() == true.split()
